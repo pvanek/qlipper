07de65ae1dc420f788394024a41f47a68105202c
stefonarch <stef.binde@riseup.net>
2015-05-23 16:08:53 +0200

    prova

---
+++ ts/qlipper_it.ts
@@ -0,0 +1,270 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sr_RS">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="147"/>
+        <source>Url: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="149"/>
+        <source>Binary: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="201"/>
+        <source>Clipboard</source>
+        <translation>Appunti</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="204"/>
+        <source>Selection</source>
+        <translation>Selezione</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="207"/>
+        <source>Find Bufer</source>
+        <translation>Trova coso</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="214"/>
+        <source>Plain Text</source>
+        <translation>Testo semplice</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="217"/>
+        <source>Rich Text</source>
+        <translation>Testo formattato</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="220"/>
+        <source>Binary Content</source>
+        <translation>Contenuto file binario</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="223"/>
+        <source>URL</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="226"/>
+        <source>Sticky Item (Plain Text)</source>
+        <translation>Voce permanente (testo semplice)</translation>
+    </message>
+</context>
+<context>
+    <name>QlipperMenuView</name>
+    <message>
+        <location filename="../src/qlippermenuview.cpp" line="36"/>
+        <source>C&amp;lear clipboard history</source>
+        <translation>&amp;Pulisci cronologia</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippermenuview.cpp" line="40"/>
+        <source>&amp;Configure...</source>
+        <translation>&amp;Configura...</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippermenuview.cpp" line="46"/>
+        <source>&amp;About Qlipper...</source>
+        <translation>&amp;A proposito di Qlipper...</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippermenuview.cpp" line="50"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Esci</translation>
+    </message>
+</context>
+<context>
+    <name>QlipperModel</name>
+    <message>
+        <location filename="../src/qlippermodel.cpp" line="186"/>
+        <source>Welcome to the Qlipper clipboard history applet</source>
+        <translation>Benvenuto a Qlipper, l&apos;applet per gli appunti</translation>
+    </message>
+</context>
+<context>
+    <name>QlipperPreferencesDialog</name>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="14"/>
+        <source>Qlipper Preferences</source>
+        <translation>Preferenze di Qlipper</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="42"/>
+        <source>Preferences</source>
+        <translation>Preferenze</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="50"/>
+        <source>Keyboard Shortcut:</source>
+        <translation>Scorciatoia di tastiera:</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="57"/>
+        <source>Change global keyboard shortcut to invoke the menu on screen</source>
+        <translation>Cambia scorciatoia gobale per mostrare il menu sullo schermo</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="79"/>
+        <source>Clipboard Entries Count:</source>
+        <translation>Numero delle voci:</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="86"/>
+        <source>The maximum count of dynamic items in the menu</source>
+        <translation>Il numero massimo delle voci da mostrare nel menu</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="92"/>
+        <source> entries</source>
+        <translation> voci</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="108"/>
+        <source>Maximum Display Size:</source>
+        <translation type="unfinished">Mostra i primi </translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="115"/>
+        <source>How long is the preview text in the menu</source>
+        <translation>Lunghezza della anteprima nel menu</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="121"/>
+        <source> characters</source>
+        <translation> caratteri</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="134"/>
+        <source>Use clipboard extensions (X11 Selections, OS X Find Buffer) when it&apos;s supported</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="137"/>
+        <source>Use Platform Specific Extensions (Advanced)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="144"/>
+        <source>Useful for bigger text blocks copied for example from dummy terminals (minicom, etc.)</source>
+        <translation>Utile per bocchi di testi grandi copiati da dummy-terminals (minicom ecc)</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="147"/>
+        <source>Trim Whitespaces for Every Line</source>
+        <translation>Rimuovi spazi vuoti per ogni riga</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="163"/>
+        <source>Sticky Items</source>
+        <translation>Voci permanenti</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="169"/>
+        <source>Sticky items are unchanged, always on top snippets in the menu</source>
+        <translation>Le voci permanenti rimangono invariati, sempre in cima nel menu</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="192"/>
+        <source>Add new item to the end of the list</source>
+        <translation>Aggiungi nuova voce in fondo alla lista</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="195"/>
+        <source>&amp;Add</source>
+        <translation>&amp;Aggiungi</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="202"/>
+        <source>Remove current item</source>
+        <translation>Rimuovi voce corrente</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="205"/>
+        <source>&amp;Remove</source>
+        <translation>&amp;Rimuovi</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="212"/>
+        <source>Move item up</source>
+        <translation>Sposta voce in su</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="215"/>
+        <source>&amp;Up</source>
+        <translation>&amp;Su</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="222"/>
+        <source>Move item down</source>
+        <translation>Sposta voce in giù</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="225"/>
+        <source>&amp;Down</source>
+        <translation>&amp;Giù</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="254"/>
+        <source>Network</source>
+        <translation> ts/qlipper_it.tsRete</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="260"/>
+        <source>Send Content to Network</source>
+        <translation>Invia contenuto in rete</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="267"/>
+        <source>Receive Content from Network</source>
+        <translation>Ricevi contenuto dalla rete</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="274"/>
+        <source>Port:</source>
+        <translation>Porta:</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.cpp" line="101"/>
+        <source>Add New Sticky Item</source>
+        <translation>Aggiungi nuova voce permanente</translation>
+    </message>
+</context>
+<context>
+    <name>QlipperSystray</name>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="52"/>
+        <source>Qlipper - a clipboard history applet</source>
+        <translation>Qlipper - un applicazione per gli appunti</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="116"/>
+        <source>Yes</source>
+        <translation>Sì</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="118"/>
+        <location filename="../src/qlippersystray.cpp" line="124"/>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="122"/>
+        <source>Yes (experimental)</source>
+        <translation>Sì (sperimentale)</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="131"/>
+        <source>About Qlipper</source>
+        <translation>A proposito di qlipper</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="133"/>
+        <source>Lightweight, cross-platform clipboard history applet.&lt;p&gt;(c)&amp;nbsp;2010-2015&amp;nbsp;Petr&amp;nbsp;Vanek&amp;nbsp;&amp;lt;petr@yarpen.cz&amp;gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pvanek/qlipper/&quot;&gt;https://github.com/pvanek/qlipper/&lt;/a&gt;&lt;p&gt;Support for global keyboard shortcut: %1&lt;br&gt;Support for network clipboard sharing: %2</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
