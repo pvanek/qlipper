dabaa2e7b983ac6da09528a94cb49e47f6a924f0
Alf Gaida <agaida@siduction.org>
2016-09-19 19:38:40 +0200

    Bump copyright year in about to 2016

--- src/qlippersystray.cpp
+++ src/qlippersystray.cpp
@@ -131,7 +131,7 @@ void QlipperSystray::showAbout()
     msgBox.setWindowTitle(tr("About Qlipper"));
     msgBox.setText(QString("<h1>Qlipper</strong> %1</h1>").arg(qApp->applicationVersion()));
     msgBox.setInformativeText(tr("Lightweight, cross-platform clipboard history applet.<p>"
-                                  "(c)&nbsp;2010-2015&nbsp;Petr&nbsp;Vanek&nbsp;&lt;petr@yarpen.cz&gt;<p>"
+                                  "(c)&nbsp;2010-2016&nbsp;Petr&nbsp;Vanek&nbsp;&lt;petr@yarpen.cz&gt;<p>"
                                   "<a href=\"https://github.com/pvanek/qlipper/\">https://github.com/pvanek/qlipper/</a>"
                                   "<p>"
                                   "Support for global keyboard shortcut: %1<br>"
