802a0f48c9e9e11d97cc22b75bb220041e4f8f91
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-03-17 13:03:15 +0100

    Add Catalan translation

---
+++ ts/qlipper_ca.ts
@@ -0,0 +1,274 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ca">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="147"/>
+        <source>Url: %1</source>
+        <translation>URL: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="149"/>
+        <source>Binary: %1</source>
+        <translation>Binari: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="201"/>
+        <source>Clipboard</source>
+        <translation>Porta-retalls</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="204"/>
+        <source>Selection</source>
+        <translation>Selecció</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="207"/>
+        <source>Find Bufer</source>
+        <translation>Memòria intermèdia de Find</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="214"/>
+        <source>Plain Text</source>
+        <translation>Text pla</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="217"/>
+        <source>Rich Text</source>
+        <translation>Text ric</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="220"/>
+        <source>Binary Content</source>
+        <translation>Contingut en binari</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="223"/>
+        <source>URL</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperitem.cpp" line="226"/>
+        <source>Sticky Item (Plain Text)</source>
+        <translation>Element exganxós (text pla)</translation>
+    </message>
+</context>
+<context>
+    <name>QlipperMenuView</name>
+    <message>
+        <location filename="../src/qlippermenuview.cpp" line="36"/>
+        <source>C&amp;lear clipboard history</source>
+        <translation>Neteja l&apos;historial del porta-reta&amp;lls</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippermenuview.cpp" line="40"/>
+        <source>&amp;Configure...</source>
+        <translation>&amp;Configura...</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippermenuview.cpp" line="46"/>
+        <source>&amp;About Qlipper...</source>
+        <translation>Qu&amp;ant a Qlipper...</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippermenuview.cpp" line="50"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Surt</translation>
+    </message>
+</context>
+<context>
+    <name>QlipperModel</name>
+    <message>
+        <location filename="../src/qlippermodel.cpp" line="186"/>
+        <source>Welcome to the Qlipper clipboard history applet</source>
+        <translation>Benvingut a la miniaplicació de l&apos;historial del porta-retalls Qlipper</translation>
+    </message>
+</context>
+<context>
+    <name>QlipperPreferencesDialog</name>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="14"/>
+        <source>Qlipper Preferences</source>
+        <translation>Preferències de Qlipper</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="42"/>
+        <source>Preferences</source>
+        <translation>Preferències</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="50"/>
+        <source>Keyboard Shortcut:</source>
+        <translation>Drecera de teclat:</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="57"/>
+        <source>Change global keyboard shortcut to invoke the menu on screen</source>
+        <translation>Canvia la drecera de teclat global per a invocar el menú en pantalla</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="79"/>
+        <source>Clipboard Entries Count:</source>
+        <translation>Nombre d&apos;entrades del porta-retalls:</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="86"/>
+        <source>The maximum count of dynamic items in the menu</source>
+        <translation>El nombre màxim d&apos;ítems dinàmics al menú</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="92"/>
+        <source> entries</source>
+        <translation> entrades</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="108"/>
+        <source>Maximum Display Size:</source>
+        <translation>Mida màxima de la visualització:</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="115"/>
+        <source>How long is the preview text in the menu</source>
+        <translation>Com de gran és el text de la vista prèvia al menú</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="121"/>
+        <source> characters</source>
+        <translation> caràcters</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="134"/>
+        <source>Use clipboard extensions (X11 Selections, OS X Find Buffer) when it&apos;s supported</source>
+        <translation>Utilitza les extensions del porta-retalls (seleccions de X11, memòria intermèdia de Find de l&apos;OS X)</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="137"/>
+        <source>Use Platform Specific Extensions (Advanced)</source>
+        <translation>Utilitza les extensions específiques de la plataforma (avançat)</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="144"/>
+        <source>Useful for bigger text blocks copied for example from dummy terminals (minicom, etc.)</source>
+        <translation>És útil per als blocs de text copiats més grans, per exemple des de terminals senzilles (minicom, etc.)</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="147"/>
+        <source>Trim Whitespaces for Every Line</source>
+        <translation>Retalla els espas en blanc per a cada línia</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="163"/>
+        <source>Sticky Items</source>
+        <translation>Elements enganxosos</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="169"/>
+        <source>Sticky items are unchanged, always on top snippets in the menu</source>
+        <translation>Els elements enganxosos estan sense canviar, sempre en la part superior dels fragments al menú</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="192"/>
+        <source>Add new item to the end of the list</source>
+        <translation>Afegeix l&apos;ítem nou al final de la llista</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="195"/>
+        <source>&amp;Add</source>
+        <translation>&amp;Afegeix</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="202"/>
+        <source>Remove current item</source>
+        <translation>Suprimeix l&apos;ítem actual</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="205"/>
+        <source>&amp;Remove</source>
+        <translation>Supr&amp;imeix</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="212"/>
+        <source>Move item up</source>
+        <translation>Mou l&apos;ítem amunt</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="215"/>
+        <source>&amp;Up</source>
+        <translation>Am&amp;unt</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="222"/>
+        <source>Move item down</source>
+        <translation>Mou l&apos;ítem avall</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="225"/>
+        <source>&amp;Down</source>
+        <translation>A&amp;vall</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="254"/>
+        <source>Network</source>
+        <translation>Xarxa</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="260"/>
+        <source>Send Content to Network</source>
+        <translation>Envia el contingut a la xarxa</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="267"/>
+        <source>Receive Content from Network</source>
+        <translation>Rep el contingut de la xarxa</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.ui" line="274"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="../src/qlipperpreferencesdialog.cpp" line="101"/>
+        <source>Add New Sticky Item</source>
+        <translation>Afegeix un element enganxós nou</translation>
+    </message>
+</context>
+<context>
+    <name>QlipperSystray</name>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="52"/>
+        <source>Qlipper - a clipboard history applet</source>
+        <translation>Qlipper - una miniaplicació de l&apos;historial del porta-retalls</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="116"/>
+        <source>Yes</source>
+        <translation>Sí</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="118"/>
+        <location filename="../src/qlippersystray.cpp" line="124"/>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="122"/>
+        <source>Yes (experimental)</source>
+        <translation>Sí (experimental)</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="131"/>
+        <source>About Qlipper</source>
+        <translation>Quant a Qlipper</translation>
+    </message>
+    <message>
+        <location filename="../src/qlippersystray.cpp" line="133"/>
+        <source>Lightweight, cross-platform clipboard history applet.&lt;p&gt;(c)&amp;nbsp;2010-2016&amp;nbsp;Petr&amp;nbsp;Vanek&amp;nbsp;&amp;lt;petr@yarpen.cz&amp;gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pvanek/qlipper/&quot;&gt;https://github.com/pvanek/qlipper/&lt;/a&gt;&lt;p&gt;Support for global keyboard shortcut: %1&lt;br&gt;Support for network clipboard sharing: %2</source>
+        <translation>Miniaplicació de l&apos;historial del porta-retalls, lleugera i de plataforma creuada.&lt;p&gt;(c)&amp;nbsp;2010-2016&amp;nbsp;Petr&amp;nbsp;Vanek&amp;nbsp;&amp;lt;petr@yarpen.cz&amp;gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pvanek/qlipper/&quot;&gt;https://github.com/pvanek/qlipper/&lt;/a&gt;&lt;p&gt;Compatibilitat per a la drecera de teclat global: %1&lt;br&gt;Compatibilitat per a la compartició del porta-retalls de xarxa: %2</translation>
+    </message>
+    <message>
+        <source>Lightweight, cross-platform clipboard history applet.&lt;p&gt;(c)&amp;nbsp;2010-2011&amp;nbsp;Petr&amp;nbsp;Vanek&amp;nbsp;&amp;lt;petr@scribus.info&amp;gt;&lt;p&gt;&lt;a href=&quot;http://code.google.com/p/qlipper/&quot;&gt;http://code.google.com/p/qlipper/&lt;/a&gt;&lt;p&gt;Support for global keyboard shortcut: %1&lt;br&gt;Support for network clipboard sharing: %2</source>
+        <translation>Miniaplicació de l&apos;historial del porta-retalls, lleugera i de plataforma creuada.&lt;p&gt;(c)&amp;nbsp;2010-2011&amp;nbsp;Petr&amp;nbsp;Vanek&amp;nbsp;&amp;lt;petr@scribus.info&amp;gt;&lt;p&gt;&lt;a href=&quot;http://code.google.com/p/qlipper/&quot;&gt;http://code.google.com/p/qlipper/&lt;/a&gt;&lt;p&gt;Compatibilitat per a la drecera de teclat global: %1&lt;br&gt;Compatibilitat per a la compartició del porta-retalls de xarxa: %2</translation>
+    </message>
+</context>
+</TS>
