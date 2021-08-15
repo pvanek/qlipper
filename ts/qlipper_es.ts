a62342eff4ff9c2a03ba48f030e8a5e4fc12ee35
micrococo <micrococo@gmx.com>
2021-08-15 12:59:44 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (69 of 69 strings)
    
    Translation: LXQt Desktop/Qlipper
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qlipper/es/

--- ts/qlipper_es.ts
+++ ts/qlipper_es.ts
@@ -30,7 +30,7 @@
     <message>
         <location filename="../src/qlipperitem.cpp" line="240"/>
         <source>Find Buffer</source>
-        <translation type="unfinished"></translation>
+        <translation>Buscar en el búfer</translation>
     </message>
     <message>
         <location filename="../src/qlipperitem.cpp" line="247"/>
