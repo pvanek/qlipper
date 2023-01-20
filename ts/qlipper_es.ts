84779364f3ddc42e80722fc50b34b9b4f4e13a6a
Joaquín <j.mir.uwu@gmail.com>
2023-01-20 21:55:11 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (69 of 69 strings)
    
    Translation: LXQt Desktop/Qlipper
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qlipper/es/

--- ts/qlipper_es.ts
+++ ts/qlipper_es.ts
@@ -171,7 +171,7 @@
     <message>
         <location filename="../src/qlipperpreferencesdialog.ui" line="209"/>
         <source>Confirm Clear History</source>
-        <translation>Confirmar el borrado del historial</translation>
+        <translation>Confirmar Limpiar Historial</translation>
     </message>
     <message>
         <location filename="../src/qlipperpreferencesdialog.ui" line="225"/>
